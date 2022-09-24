9f172228ae99414d2876061e72a74080f4b10d18
guiled <guislain.duthieuw@gmail.com>
2022-09-24 15:03:51 +0200

    New way to compile project

---
+++ build.ts
@@ -0,0 +1,40 @@
+import esbuild from "esbuild";
+import { transformFile, transformSync } from "@swc/core";
+import { transformForLR, noVoid0Plugin } from "./transpile.conf";
+import fs from "fs";
+
+let swcPlugin: esbuild.Plugin = {
+  name: "swcPlugin",
+  setup(build) {
+    build.onLoad({ filter: /\.ts?$/ }, async (args) => {
+      let input = await fs.promises.readFile(args.path, "utf8");
+      //var output = transformFileSync(args.path, transformForLR);
+      var output = transformSync(input, transformForLR);
+      return {
+        contents: output.code,
+        loader: "ts",
+      };
+    });
+  },
+};
+
+esbuild
+  .build({
+    entryPoints: ["src/index.ts"],
+    target: "es2022",
+    bundle: true,
+    minify: false,
+    platform: "neutral",
+    outfile: "build/lre.tmp.js",
+    plugins: [swcPlugin],
+    format: "iife",
+    define: {
+      "console.log": "log",
+    },
+  })
+  .then(() => transformFile("build/lre.tmp.js", noVoid0Plugin))
+  .then((result) => fs.writeFileSync("build/lre.js", `//region LRE ${process.env.npm_package_version}
+${result.code.trim()}
+//endregion LRE ${process.env.npm_package_version}
+`));
+
--- transpile.conf.ts
+++ transpile.conf.ts
@@ -1,40 +1,40 @@
 import { Options, plugins } from "@swc/core";
 import classExtend from "./src/swc/ClassExtend";
 import classToFunction from "./src/swc/ClassToFunction";
-import defaultVisitor from "./src/swc/DefaulVisitor";
+import defaultVisitor from "./src/swc/DefaultVisitor";
+import keepInstanceOf from "./src/swc/KeepInstanceOf";
 import noArraySpreading from "./src/swc/NoArraySpreading";
 import noVoid0 from "./src/swc/NoVoid0";
 
-const conf: Options = {
+const transformForLR: Options = {
   jsc: {
     parser: {
-      syntax: "ecmascript",
+      syntax: "typescript",
+      tsx: false,
     },
     target: "es5",
     loose: true,
   },
   minify: false,
+  isModule: true,
   module: {
-    type: "commonjs",
-    strict: false,
-    strictMode: false,
-    lazy: false,
-    noInterop: false,
-    ignoreDynamic: false,
+    type: "es6",
+    // strict: false,
+    // strictMode: false,
+    // lazy: false,
+    // noInterop: false,
+    // ignoreDynamic: false,
   },
-  plugin: plugins([
-    noArraySpreading(),
-    classExtend(),
-    classToFunction(),
-  ]),
+  //plugin: plugins([defaultVisitor()]),
+  //plugin: plugins([defaultVisitor(), classToFunction()]),
+  plugin: plugins([noArraySpreading(), classExtend(), classToFunction()]),
   sourceMaps: false,
 };
 
-const noVoid0Plugin = {
-  ...conf,
-  plugin: plugins([noVoid0()]),
+const noVoid0Plugin: Options = {
+  ...transformForLR,
+  plugin: plugins([noVoid0(), keepInstanceOf()]),
 };
 
-export { noVoid0Plugin };
-
-export default conf;
+export { transformForLR, noVoid0Plugin };
+//, noVoid0Plugin};
