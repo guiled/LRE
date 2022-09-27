295c1f3063cdd67635fbb7214bd51fbddcea87ee
guiled <guislain.duthieuw@gmail.com>
2022-09-28 00:38:21 +0200

    Compiler replace in operator

---
+++ src/swc/ReplaceInOperator.ts
@@ -0,0 +1,38 @@
+import {
+  Expression,
+  Program,
+  TsType,
+} from "@swc/core";
+import Visitor from "@swc/core/Visitor";
+import call from "./node/expression/call";
+import member from "./node/expression/member";
+import identifier from "./node/identifier";
+
+class ReplaceInOperator extends Visitor {
+
+  visitExpression(n: Expression): Expression {
+    if (n.type === "BinaryExpression" && n.operator === "in") {
+      return super.visitExpression(call({
+        span: n.span,
+        callee: member({
+          span: n.span,
+          object: n.right,
+          property: identifier({
+            span: n.span,
+            value: "hasOwnProperty"
+          }),
+        }),
+        args: [{expression: n.left}],
+      }));
+    }
+    return super.visitExpression(n);
+  }
+
+  visitTsType(n: TsType): TsType {
+    return n;
+  }
+}
+
+export default function replaceInOperator() {
+  return (program: Program) => new ReplaceInOperator().visitProgram(program);
+}
