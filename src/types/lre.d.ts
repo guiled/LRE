6086bf8c2f94fbe18b3fca61b4335e09a58b5a9f
guiled <guislain.duthieuw@gmail.com>
2023-06-11 16:33:55 +0200

    Create many components in ts

--- src/types/lre.d.ts
+++ src/types/lre.d.ts
@@ -1,9 +1,17 @@
-declare interface ILRE  {
-    error(...args: any[]): void;
-    warn(...args: any[]): void;
-    trace(...args: any[]): void;
-
+declare interface ILRE {
+  error(...args: any[]): void;
+  warn(...args: any[]): void;
+  trace(...args: any[]): void;
 }
 
 declare var lre: ILRE & Logger;
 declare var firstInit: (sheet: Sheet) => boolean;
+
+declare type ComponentType =
+  | "component"
+  | "sheet"
+  | "repeater"
+  | "choice"
+  | "multichoice"
+  | "entry"
+  | "icon";
