a99606fea59bb1c1683c0cddff7f43da6fe1c141
guiled <guislain.duthieuw@gmail.com>
2023-08-10 00:03:44 +0200

    Dataholder full test

---
+++ src/dataholder/dataholder.ts
@@ -0,0 +1,19 @@
+import { Sheet } from "../sheet";
+
+type DataId = string;
+
+export class DataHolder {
+  #data: Record<DataId, any> = {};
+
+  hasData(name: DataId): boolean {
+    return this.#data.hasOwnProperty(name);
+  }
+
+  setData(name: DataId, value: any): void {
+    this.#data[name] = value;
+  }
+
+  getData(name: DataId): any {
+    return this.#data[name];
+  }
+}
