fe5fa89394392347581021e7bc1889d28572c975
guiled <guislain.duthieuw@gmail.com>
2023-10-13 23:51:02 +0200

    Test LRE bootstrap

---
+++ src/bootstrap.ts
@@ -0,0 +1,33 @@
+import { LRE } from "./lre";
+import { overloadTables } from "./tables";
+
+
+export const bootstrap = () => {
+  // @ts-ignore Define isNaN because it is missing in Let's Role
+  isNaN = function (val: any): boolean {
+    return Number.isNaN(Number(val));
+  };
+
+  // @ts-ignore Define structuredClone because it is missing in Let's Role
+  structuredClone = function (val): any {
+    if (typeof val === "object" || Array.isArray(val)) {
+      let result: any = Array.isArray(val) ? [] : {};
+      each(val, function (v, k) {
+        result[k] = structuredClone(v);
+      });
+      return result;
+    } else {
+      return val;
+    }
+  };
+
+  overloadTables(Tables);
+  lre = new LRE();
+  // @ts-ignore Overload console for some edge cases like throw new Error changed into console.error
+  console = {
+    log: lre.log,
+    error: lre.error,
+    trace: lre.log,
+    warn: lre.warn,
+  };
+};
