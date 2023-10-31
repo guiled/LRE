4d6a43ae20baf9fff0bcaeb45e62f3d7b38ebd4a
guiled <guislain.duthieuw@gmail.com>
2023-11-01 00:16:59 +0100

    Transpile exceptions

--- src/swc/node/expression/call.ts
+++ src/swc/node/expression/call.ts
@@ -1,13 +1,31 @@
-import { CallExpression, Span } from "@swc/core";
+import { CallExpression, OptionalChainingCall, OptionalChainingExpression, Span } from "@swc/core";
 
 export type Call_Param = {
   span: Span;
   callee: CallExpression["callee"];
   args?: CallExpression["arguments"];
-  typeArguments?: CallExpression["typeArguments"]
+  typeArguments?: CallExpression["typeArguments"];
 };
 
-export default ({ span, callee, args = [], typeArguments }: Call_Param): CallExpression => {
+export default (
+  { span, callee, args = [], typeArguments }: Call_Param,
+  optional: boolean = false
+): CallExpression | OptionalChainingExpression => {
+  
+  if (optional) {
+    return {
+      type: "OptionalChainingExpression",
+      span,
+      questionDotToken: span,
+      base: {
+        type: "CallExpression",
+        span,
+        callee: callee as OptionalChainingCall["callee"],
+        arguments: args,
+        typeArguments,
+      },
+    }
+  }
   return {
     type: "CallExpression",
     span,
--- src/swc/node/expression/member.ts
+++ src/swc/node/expression/member.ts
@@ -1,16 +1,28 @@
-import { MemberExpression, Span } from "@swc/core";
+import { MemberExpression, OptionalChainingExpression, Span } from "@swc/core";
 
 type Member_Type = {
-    span: Span,
-    object: MemberExpression["object"],
-    property: MemberExpression["property"]
-}
-
-export default function member({span, object, property}: Member_Type): MemberExpression {
-    return {
-        type: "MemberExpression",
-        span,
-        object,
-        property
-    }
+  span: Span;
+  object: MemberExpression["object"];
+  property: MemberExpression["property"];
 };
+
+export default function member(
+  { span, object, property }: Member_Type,
+  optional: boolean = false
+): MemberExpression | OptionalChainingExpression {
+  const expression: MemberExpression = {
+    type: "MemberExpression",
+    span,
+    object,
+    property,
+  };
+  if (optional) {
+    return {
+      type: "OptionalChainingExpression",
+      questionDotToken: span,
+      span,
+      base: expression,
+    };
+  }
+  return expression;
+}
