33ec59581ef4d190fa4cb0c2a0f1f7ce03e60c3c
guiled <guislain.duthieuw@gmail.com>
2023-11-15 00:54:03 +0100

    implement lre.deepEqual

--- src/lre.ts
+++ src/lre.ts
@@ -110,4 +110,25 @@ export class LRE extends Logger implements ILRE {
 
     return this.deepMerge.apply(this, [target, ...sources]);
   }
+
+  deepEqual(x: any, y: any): boolean {
+    if (x === y) {
+      return true;
+    } else if (
+      typeof x == "object" &&
+      x != null &&
+      typeof y == "object" &&
+      y != null
+    ) {
+      if (Object.keys(x).length != Object.keys(y).length) return false;
+
+      for (var prop in x) {
+        if (y.hasOwnProperty(prop)) {
+          if (!this.deepEqual(x[prop], y[prop])) return false;
+        } else return false;
+      }
+
+      return true;
+    } else return false;
+  }
 }
