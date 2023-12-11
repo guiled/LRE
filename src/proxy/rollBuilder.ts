d5cbcf71e30ac007f9df50f23aac98a71fa04672
guiled <guislain.duthieuw@gmail.com>
2023-12-11 17:43:43 +0100

    Implement RollBuilder proxy

---
+++ src/proxy/rollBuilder.ts
@@ -0,0 +1,52 @@
+export const registerLreRollBuilder = (
+  modeHandler: ProxyModeHandler,
+  initialRollBuilder: typeof RollBuilder
+): typeof RollBuilder =>
+  class {
+    #raw: RollBuilder;
+    constructor(sheet: LetsRole.Sheet) {
+      this.#raw = new initialRollBuilder(sheet);
+    }
+
+    public roll() {
+      if (modeHandler.getMode() === "real") {
+        this.#raw.roll();
+      }
+    }
+
+    public expression(expr: string) {
+      this.#raw.expression(expr);
+
+      return this;
+    }
+
+    public title(title: string) {
+      this.#raw.title(title);
+
+      return this;
+    }
+
+    public visibility(visibility: LetsRole.RollVisibility) {
+      this.#raw.visibility(visibility);
+
+      return this;
+    }
+
+    public addAction(title: string, callback: (...args: any[]) => void) {
+      this.#raw.addAction(title, callback);
+
+      return this;
+    }
+
+    public removeAction(title: string) {
+      this.#raw.removeAction(title);
+
+      return this;
+    }
+
+    public onRoll(callback: (...args: any[]) => void) {
+      this.#raw.onRoll(callback);
+
+      return this;
+    }
+  };
