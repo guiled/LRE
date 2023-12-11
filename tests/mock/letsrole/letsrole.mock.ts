c8f17c30ca11561839cd016395834cb50c39890e
guiled <guislain.duthieuw@gmail.com>
2023-12-11 17:43:43 +0100

    Implement RollBuilder proxy

--- tests/mock/letsrole/letsrole.mock.ts
+++ tests/mock/letsrole/letsrole.mock.ts
@@ -2,6 +2,16 @@ import { newMockedWait } from "./wait.mock";
 
 const { wait, itHasWaitedEnough, itHasWaitedEverything } = newMockedWait();
 
+const rollBuilderMock = {
+  roll: jest.fn(),
+  expression: jest.fn(),
+  title: jest.fn(),
+  visibility: jest.fn(),
+  addAction: jest.fn(),
+  removeAction: jest.fn(),
+  onRoll: jest.fn(),
+};
+
 const initLetsRole = () => {
   global.each = jest.fn((obj: any, cb) => {
     for (let k in obj) {
@@ -10,13 +20,29 @@ const initLetsRole = () => {
   });
 
   global.wait = wait;
-  
+
   global.Bindings = {
     add: jest.fn(),
     clear: jest.fn(),
     remove: jest.fn(),
     send: jest.fn(),
   } as LetsRole.Bindings;
+
+  /* @ts-ignore */
+  global.RollBuilder = class {
+    roll = rollBuilderMock.roll;
+    expression = rollBuilderMock.expression;
+    title = rollBuilderMock.title;
+    visibility = rollBuilderMock.visibility;
+    addAction = rollBuilderMock.addAction;
+    removeAction = rollBuilderMock.removeAction;
+    onRoll = rollBuilderMock.onRoll;
+  };
 };
 
-export { initLetsRole, itHasWaitedEnough, itHasWaitedEverything };
+export {
+  initLetsRole,
+  itHasWaitedEnough,
+  itHasWaitedEverything,
+  rollBuilderMock,
+};
