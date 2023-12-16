14edeb478e36a498cdf1e77b40dbc16d0c3131b7
guiled <guislain.duthieuw@gmail.com>
2023-12-16 22:55:30 +0100

    Fix transpile of computed object props that used this

--- src/swc/ComputedObjectProp.ts
+++ src/swc/ComputedObjectProp.ts
@@ -1,8 +1,11 @@
 import {
+  Class,
   ComputedPropName,
   Expression,
+  Fn,
   KeyValueProperty,
   Program,
+  ThisExpression,
   TsType,
 } from "@swc/core";
 import { Visitor } from "@swc/core/Visitor";
@@ -14,52 +17,79 @@ import assignment from "./node/statement/assignment";
 import returnstmt from "./node/statement/returnstmt";
 
 class ComputedObjectProps extends Visitor {
+  protected hasThisExpression: boolean = false;
+
+  visitThisExpression(n: ThisExpression): Expression {
+    this.hasThisExpression = true;
+    return super.visitThisExpression(n);
+  }
+
+  visitFunction<T extends Fn>(n: T): T {
+    const saveHasThisExpression = this.hasThisExpression;
+    const result = super.visitFunction(n);
+    this.hasThisExpression = saveHasThisExpression;
+    return result;
+  }
+
+  visitClass<T extends Class>(n: T): T {
+    const saveHasThisExpression = this.hasThisExpression;
+    const result = super.visitClass(n);
+    this.hasThisExpression = saveHasThisExpression;
+    return result;
+  }
 
   visitExpression(n: Expression): Expression {
-
+    let expr = n;
     if (n.type === "ObjectExpression") {
       const computedProps: KeyValueProperty[] = n.properties.filter(
         (p) => p.type === "KeyValueProperty" && p.key.type === "Computed"
       ) as KeyValueProperty[];
       if (computedProps.length > 0) {
         const tmpObj = identifier({ span: n.span, value: "o" });
-        return super.visitExpression(iife({
-          span: n.span,
-          stmts: [
-            onevariable({
-              span: n.span,
-              id: tmpObj,
-              init: {
-                ...n,
-                properties: n.properties.filter(
-                  (p) =>
-                    !(
-                      p.type === "KeyValueProperty" && p.key.type === "Computed"
-                    )
-                ),
-              },
-            }),
-            ...computedProps.map((p: KeyValueProperty) =>
-              assignment({
-                span: p.key.span,
-                left: member({
-                  span: p.key.span,
-                  object: tmpObj,
-                  property: p.key as ComputedPropName,
-                }),
-                right: p.value,
-                operator: "=",
-              })
-            ),
-            returnstmt({
-              span: n.span,
-              argument: tmpObj,
-            }),
-          ],
-        }));
+        const stmts = [
+          onevariable({
+            span: n.span,
+            id: tmpObj,
+            init: {
+              ...n,
+              properties: n.properties.filter(
+                (p) =>
+                  !(p.type === "KeyValueProperty" && p.key.type === "Computed")
+              ),
+            },
+          }),
+          ...computedProps.map((p: KeyValueProperty) => {
+            const key = this.visitComputedPropertyKey(
+              p.key as ComputedPropName
+            );
+            const val = this.visitExpression(p.value);
+            return assignment({
+              span: key.span,
+              left: member({
+                span: key.span,
+                object: tmpObj,
+                property: key,
+              }),
+              right: val,
+              operator: "=",
+            });
+          }),
+          returnstmt({
+            span: n.span,
+            argument: tmpObj,
+          }),
+        ];
+        expr = super.visitExpression(
+          iife({
+            span: n.span,
+            stmts,
+            called: this.hasThisExpression,
+          })
+        );
       }
     }
-    return super.visitExpression(n);
+    const result = super.visitExpression(expr);
+    return result;
   }
 
   visitTsType(n: TsType): TsType {
