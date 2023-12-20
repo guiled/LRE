8f1e7644afef6dd3cd7b3494ffe067beadb5ab12
guiled <guislain.duthieuw@gmail.com>
2023-12-20 22:12:22 +0100

    Move ComponentCommon to definition file

--- src/component/Icommon.ts
+++
@@ -1,9 +0,0 @@
-import { Sheet } from "../sheet";
-
-export interface ComponentCommon {
-  lreType: (newType?: ComponentType) => ComponentType;
-  sheet: () => Sheet;
-  realId: () => string;
-  //entry: (entry?: Entry) => Entry | undefined;
-  //repeater: (repeater?: Repeater) => Repeater | undefined;
-}
