dbc3d854b923e229344f45acb4532014cefafccc
guiled <guislain.duthieuw@gmail.com>
2024-01-28 10:18:31 +0100

    Add choice component first feature

--- src/component/choice.ts
+++ src/component/choice.ts
@@ -1,5 +1,45 @@
-import {Component} from ".";
+import { Component } from ".";
+import { dynamicSetter } from "../globals/decorators/dynamicSetter";
 
-export class Choice extends Component {
+type ChoiceEvents =
+  | "select"
+  | "valselect"
+  | "unselect"
+  | "valunselect"
+  | "valclick";
+export class Choice extends Component<LetsRole.ComponentValue, ChoiceEvents> {
+  //#choices: LetsRole.Choices = {};
+  #currentValue: LetsRole.ComponentValue | void;
 
+  constructor(
+    raw: LetsRole.Component,
+    sheet: ISheet,
+    realId: LetsRole.ComponentID
+  ) {
+    super(raw, sheet, realId);
+    this.lreType("choice");
+    try {
+      this.#currentValue = lre.value(raw.value());
+    } catch (e) {
+      lre.error(`[Choice] Unable to get ${realId} value. ${e}`);
+    }
+    this.on("update:checkChanges", this.#checkChanges);
+  }
+
+  #checkChanges(choice: Component) {
+    const newValue = choice.value()!;
+    if (newValue !== this.#currentValue) {
+      this.trigger(`valselect:${newValue}`);
+      this.trigger("select", newValue);
+      this.trigger(`valunselect:${this.#currentValue}`);
+      this.trigger("unselect", this.#currentValue);
+    }
+    this.trigger(`valclick:${newValue}`);
+    this.#currentValue = newValue;
+  }
+
+  @dynamicSetter
+  setChoices(_choices?: LetsRole.Choices): void {
+    super.setChoices.apply(this, Array.from(arguments) as [choices: LetsRole.Choices]);
+  }
 }
