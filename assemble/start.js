2098199e2e9720a188e4f90b35a879c034f7d6e2
Guiled <guislain.duthieuw@gmail.com>
2024-05-27 00:33:17 +0200

    Fix a conflict with minified built code

--- assemble/start.js
+++ assemble/start.js
@@ -1,7 +1,7 @@
 
     let context;
     let errExclFirstLine, errExclLastLine;
-    try { let a = null; a() } catch (e) { errExclFirstLine = e.trace[0].loc.start.line };
+    try { let a42 = null; a42() } catch (e) { errExclFirstLine = e.trace[0].loc.start.line };
 
     function instanceOf(a, b) {
         return a instanceof b;
