94ce35fc2912f9b4e492a46419dd66b64706f8b4
Guiled <guislain.duthieuw@gmail.com>
2025-01-17 23:18:51 +0100

    Add choices use case on chained transformed DataProvider

--- usecases/choices.test.ts
+++ usecases/choices.test.ts
@@ -10,6 +10,7 @@ import { DataBatcher } from "../src/sheet/databatcher";
 import { SheetProxy } from "../src/proxy/sheet";
 import { ViewMock } from "../src/mock/letsrole/view.mock";
 import { Choice } from "../src/component/choice";
+import { DirectDataProvider } from "../src/dataprovider";
 
 let server: ServerMock;
 let sheetMock: ViewMock;
@@ -33,6 +34,21 @@ beforeEach(() => {
             className: "Choice",
             tableId: "skills",
             label: "name",
+            multiple: true,
+          },
+          {
+            id: "selectedSkills",
+            className: "Choice",
+            tableId: "skills",
+            label: "name",
+            multiple: true,
+          },
+          {
+            id: "selectedTransformedSkills",
+            className: "Choice",
+            tableId: "skills",
+            label: "name",
+            multiple: true,
           },
         ],
       },
@@ -45,15 +61,30 @@ beforeEach(() => {
         { id: "bard", name: "bard" },
       ],
       skills: [
-        { id: "sword", name: "Sword", class: "warrior" },
-        { id: "axe", name: "Axe", class: "warrior" },
-        { id: "2-hand sword", name: "Two-hand sword", class: "warrior" },
-        { id: "dagger", name: "Dagger", class: "rogue" },
-        { id: "bow", name: "Bow", class: "rogue" },
-        { id: "fireball", name: "Fireball", class: "mage" },
-        { id: "invisibility", name: "Invisibility", class: "mage" },
-        { id: "charm", name: "Charm", class: "bard" },
-        { id: "song", name: "Song", class: "bard" },
+        { id: "sword", name: "Sword", class: "warrior", otherName: "Blade" },
+        { id: "axe", name: "Axe", class: "warrior", otherName: "Hatchet" },
+        {
+          id: "2-hand sword",
+          name: "Two-hand sword",
+          class: "warrior",
+          otherName: "Great sword",
+        },
+        { id: "dagger", name: "Dagger", class: "rogue", otherName: "Knife" },
+        { id: "bow", name: "Bow", class: "rogue", otherName: "Longbow" },
+        {
+          id: "fireball",
+          name: "Fireball",
+          class: "mage",
+          otherName: "Flame orb",
+        },
+        {
+          id: "invisibility",
+          name: "Invisibility",
+          class: "mage",
+          otherName: "Stealth cloak",
+        },
+        { id: "charm", name: "Charm", class: "bard", otherName: "Allure" },
+        { id: "song", name: "Song", class: "bard", otherName: "Melody" },
       ],
     },
   });
@@ -97,6 +128,61 @@ describe("Set choices with table", () => {
     expect(choiceSkills.getChoices()).toStrictEqual({
       fireball: "Fireball",
       invisibility: "Invisibility",
+    });
+  });
+
+  test("Dynamic set choices based on a transformed provider", () => {
+    sheetMock.componentChangedManually("class", "warrior");
+
+    const skills = Tables.get("skills")!;
+
+    const selectedClass = sheet.get("class") as Choice;
+
+    const choiceSkills = sheet.get("skills") as Choice;
+
+    choiceSkills.setChoices(
+      skills.select("name").where("class", selectedClass),
+    );
+
+    selectedClass.value("warrior");
+
+    choiceSkills.value(["sword", "2-hand sword"]);
+
+    const selectedSkills = sheet.get("selectedSkills") as Choice;
+
+    selectedSkills.setChoices(choiceSkills.checked());
+
+    expect(selectedSkills.getChoices()).toStrictEqual({
+      sword: "Sword",
+      "2-hand sword": "Two-hand sword",
+    });
+
+    const selectedTransformedSkills = sheet.get(
+      "selectedTransformedSkills",
+    ) as Choice;
+
+    const transformed = choiceSkills
+      .checked()
+      .transform("otherName") as DirectDataProvider;
+
+    selectedTransformedSkills.setChoices(transformed);
+
+    expect(selectedTransformedSkills.getChoices()).toStrictEqual({
+      sword: "Blade",
+      "2-hand sword": "Great sword",
+    });
+
+    choiceSkills.value(["sword", "2-hand sword", "axe"]);
+
+    expect(selectedSkills.getChoices()).toStrictEqual({
+      sword: "Sword",
+      "2-hand sword": "Two-hand sword",
+      axe: "Axe",
+    });
+    expect(selectedTransformedSkills.getChoices()).toStrictEqual({
+      sword: "Blade",
+      "2-hand sword": "Great sword",
+      axe: "Hatchet",
     });
   });
 });
