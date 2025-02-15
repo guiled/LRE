0cf85e4bf3cce992219fcd74ab4a1c4505e8cf73
Guiled <guislain.duthieuw@gmail.com>
2025-02-15 14:16:26 +0100

    Improve entry find

--- src/component/entry.ts
+++ src/component/entry.ts
@@ -1,8 +1,15 @@
-import { Component } from "./component";
+import { Component, REP_ID_SEP } from "./component";
 
 export class Entry extends Component<LetsRole.ViewData> {
   constructor(raw: LetsRole.Component, sheet: ISheet, realId: string) {
     super(raw, sheet, realId);
     this.lreType("entry");
+  }
+
+  find(id: LetsRole.ComponentID, silent: boolean = false): Component | null {
+    return this.sheet().get(
+      this.realId() + REP_ID_SEP + id,
+      silent,
+    ) as Component | null;
   }
 }
